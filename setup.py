b0fe1b936bf178f00bcf3863bc505a22b268a476
John Scanlon <john.t.scanlon.2@gmail.com>
2024-08-13 22:57:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read_requirements():
 
 setuptools.setup(
     name='pandas-sigproc',
-    version='1.0.4',
+    version='1.0.5',
     description='Useful pandas extensions for detailed signal processing',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
